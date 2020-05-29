edbbf78c474992c74a1d76d07e2ff86c2b667de1
Bao Yinxing <byhang@forwardx.com>
2020-05-29 20:33:54 +0800

    stonger

--- box_4keypoints_labeler.py
+++ box_4keypoints_labeler.py
@@ -6,6 +6,7 @@ import time
 import numpy as np
 from os import listdir
 from os.path import isfile, join
+from send2trash import send2trash
 
 currentClassID = 0
 tmpPoints = []
@@ -49,7 +50,7 @@ def xywh2xy(box, flag):
         return (int(box[0] + box[2] / 2), int(box[1] + box[3] / 2))
     
 
-def drow_box_with_4points(image, label, fontScale = 1.1):
+def drow_box_with_4points(image, label, fontScale = 0.7):
     reduction_label = label.copy()
     reduction_label[1:13:2] = reduction_label[1:13:2] * image.shape[1]
     reduction_label[2:13:2] = reduction_label[2:13:2] * image.shape[0]
@@ -59,32 +60,33 @@ def drow_box_with_4points(image, label, fontScale = 1.1):
     for i in range(5,13,2):
         x = reduction_label[i]
         y = reduction_label[i+1]
-        cv2.circle(image, (int(x), int(y)), 5, (0, 255, 0), thickness=-1)
+        cv2.circle(image, (int(x), int(y)), 4, (0, 255, 0), thickness=-1)
     cv2.putText(image, '{}'.format(className[int(reduction_label[0])]), xywh2xy(box, 2), cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale, (255, 0, 0), thickness=1)
 
 
-def drawing(image, image_path, label_path):
+def drawing(image, image_path, label_path, label_loaded):
     global tmpLabels, tmpPoints
-    fontScale = 1
+    fontScale = 0.65
+    thickness = 1
+    line_h = 16 #pixel
     image_show = image.copy()
     if g_print_help:
-        p = image_show.shape[0] - 3
-        line_h = 22 #pixel
-        for i in range(len(className)):
-            if i == currentClassID:
-                cv2.putText(image_show, '{}:{}'.format(i, className[i]), (0, p - i * line_h), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0), thickness=1)
-            else:
-                cv2.putText(image_show, '{}:{}'.format(i, className[i]), (0, p - i * line_h), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 255), thickness=1)
-        text = "[h]:print this help,[Esc]:exit,[0~9]:class,[ ]:save,[w]:next,[q]:previous,[c]:clear,[b]:undo,[Del/d]:del"
+        text = "[h]:print this help,[Esc]:exit,[0~9]:class,[ ]:save,[w]:next,[q]:previous,[r]:undo,[Del/d]:del"
         words = text.split(',')
         for i in range(len(words)):
-            cv2.putText(image_show, words[i], (0, (i + 1) * line_h), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 255), thickness=1)
+            cv2.putText(image_show, words[i], (0, (i + 1) * line_h), cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale, (0, 255, 255), thickness)
     else:
-        cv2.putText(image_show, '[-h] for help', (0, image_show.shape[0] - 3), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 255), thickness=1)
-
-    if isfile(label_path):
+        cv2.putText(image_show, '[-h] for help', (0, line_h), cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale, (0, 255, 255), thickness)
+    p = image_show.shape[0] - 3
+    for i in range(len(className)):
+        if i == currentClassID:
+            cv2.putText(image_show, '{}:{}'.format(i, className[i]), (0, p - i * line_h), cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale, (0, 255, 0), thickness)
+        else:
+            cv2.putText(image_show, '{}:{}'.format(i, className[i]), (0, p - i * line_h), cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale, (0, 255, 255), thickness)
+    
+    if isfile(label_path) and not label_loaded[0]:
         tmpLabels = np.loadtxt(label_path).reshape(-1, 13)
-        # label : class x y w h x1 y1 x2 y2 x3 y3 x4 y4
+        label_loaded[0] = True
         for i in range(tmpLabels.shape[0]):
             drow_box_with_4points(image_show, tmpLabels[i, ...], fontScale)
     else:
@@ -96,7 +98,6 @@ def drawing(image, image_path, label_path):
             x = tmpPoints[i]
             y = tmpPoints[i+1]
             cv2.circle(image_show, (int(x), int(y)), 3, (255, 0, 255), thickness=-1)
-            cv2.putText(image_show, 'No.{}:({},{})'.format(i // 2, x, y), (int(x), int(y)), cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale, (255, 0, 0), thickness=1)
             i += 2
     cv2.imshow("image", image_show)
 
@@ -133,7 +134,7 @@ def on_EVENT_LBUTTONDOWN(event, x, y, flags, param):
             else:
                 tmpLabels = np.vstack((tmpLabels, tmp))
             tmpPoints = []
-        drawing(param[0], param[1], param[2])
+        drawing(param[0], param[1], param[2], param[3])
 
 def soft_resize(image, size):
     h, w, _ = image.shape
@@ -150,17 +151,14 @@ def soft_resize(image, size):
 def main(args):
     global currentClassID, tmpLabels, tmpPoints, g_image_size, g_print_help
     if args.skip:
-        original_images = [f for f in listdir(args.original_image_dir) if isfile(join(args.original_image_dir, f))]
         labeled_images = [f for f in listdir(args.image_dir) if isfile(join(args.image_dir, f))]
-        # labeled_images_without_suffix = [f.split('.')[0] for f in labeled_images]
-        # image_files = [f for f in original_images if f.split('.')[0] not in labeled_images_without_suffix]
-        image_files = list(set(original_images).difference(set(labeled_images)))
+        image_files = [f for f in listdir(args.original_image_dir) if f not in labeled_images and isfile(join(args.original_image_dir, f))]
     else:
         image_files = [f for f in listdir(args.original_image_dir) if isfile(join(args.original_image_dir, f))]
-
+    image_files.sort()
     cv2.namedWindow("image")
     imgIdx = 0
-    image_count = 0
+    image_count = 1
     image_num = len(image_files)
     label_duration = 0
     while len(image_files) > 0 and imgIdx >= 0 and imgIdx < len(image_files):
@@ -177,28 +175,33 @@ def main(args):
 
         image = cv2.imread(original_image_path)
         if image is None:
-            print(image_name, "is Error!")
-            os.remove(original_image_path)
+            # os.remove(original_image_path)
+            send2trash(original_image_path)
             image_files.remove(image_name)
             continue
         
         resized_image = soft_resize(image, g_image_size)
-        cv2.setMouseCallback("image", on_EVENT_LBUTTONDOWN, param=[resized_image, labeled_image_path, label_path])
-        drawing(resized_image, labeled_image_path, label_path)
+        label_loaded = [False]
+        cv2.setMouseCallback("image", on_EVENT_LBUTTONDOWN, param=[resized_image, labeled_image_path, label_path, label_loaded])
+        drawing(resized_image, labeled_image_path, label_path, label_loaded)
         while True:
-            key = cv2.waitKey()    # "[h]:help [Esc]:exit [0~9]:class [ ]:save [w]:next [q]:previous [c]:clear [b]:undo [Del d]:delete"
-            # if -1 != key:
-            #     print(key)
+            key = cv2.waitKey()
+            if -1 != key:
+                print(key)
             
             if 27 == key: # 'Esc'
                 return
             
             if 48 <= key and len(className) + 48 > key: # 0~9
                 currentClassID = key - 48
-                drawing(resized_image, labeled_image_path, label_path)
+                # if len(tmpPoints) == 0 and tmpLabels.size:
+                #     tmpLabels[-1, 0] = currentClassID
+                drawing(resized_image, labeled_image_path, label_path, label_loaded)
 
             if 32 == key: # /space
                 cv2.imwrite(labeled_image_path, resized_image)
+                if not tmpLabels.size:
+                    continue
                 np.savetxt(label_path, tmpLabels, fmt = g_label_fmt)
                 imgIdx += 1
                 break
@@ -217,17 +220,7 @@ def main(args):
                     print("Has returned to the tail.")
                 break
 
-            if 99 == key: # clear
-                tmpLabels = np.empty(0)
-                tmpPoints = []
-                if isfile(label_path):
-                    os.remove(label_path)
-                if isfile(labeled_image_path):
-                    os.remove(labeled_image_path)
-                drawing(resized_image, labeled_image_path, label_path)
-                break
-
-            if 98 == key: # back
+            if 114 == key: # undo
                 if len(tmpPoints) >= 2:
                     tmpPoints = tmpPoints[:-2]
                 elif tmpLabels.size:
@@ -236,27 +229,27 @@ def main(args):
                     tmpPoints[0:6:2] = (tmpPoints[0:6:2] * resized_image.shape[1]) + 0.5
                     tmpPoints[1:6:2] = (tmpPoints[1:6:2] * resized_image.shape[0]) + 0.5
                     tmpPoints = tmpPoints.astype(np.int32).tolist()
-                drawing(resized_image, labeled_image_path, label_path)
+                drawing(resized_image, labeled_image_path, label_path, label_loaded)
 
             if 255 == key or 100 == key: # del image
-                os.remove(original_image_path)
+                # os.remove(original_image_path)
+                send2trash(original_image_path)
                 if isfile(labeled_image_path):
-                    os.remove(labeled_image_path)
+                    # os.remove(labeled_image_path)
+                    send2trash(labeled_image_path)
                 if isfile(label_path):
-                    os.remove(label_path)
+                    # os.remove(label_path)
+                    send2trash(label_path)
                 image_files.remove(image_name)
                 print("--Removed", original_image_path)
                 break
             
             if 104 == key: # print help ?
                 g_print_help = (g_print_help + 1) % 2
-                drawing(resized_image, labeled_image_path, label_path)
+                drawing(resized_image, labeled_image_path, label_path, label_loaded)
         tok = time.time()
         duration = tok - tik
-        if imgIdx == 0:
-            label_duration = duration
-            image_count += 1
-        elif duration <= 15:
+        if duration <= 30:
             label_duration += duration
             image_count += 1
         print('当前为第 {} 张图片，已用时 {:.2f} 分钟'.format(imgIdx+1, label_duration/60))
